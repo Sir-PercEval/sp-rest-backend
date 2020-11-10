2f5fbd4e96d4d335903659e08381826b9a588ca7
karsten <kgresch@redhat.com>
2020-11-10 08:07:06 +0000

    Events

--- service-people/src/main/java/org/perceval/backend/PeopleResource.java
+++ service-people/src/main/java/org/perceval/backend/PeopleResource.java
@@ -5,12 +5,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/people")
 public class PeopleResource {
 
+    // TODO: Get list, put 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
     }
+
+
 }
